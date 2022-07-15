7dcaac58606c49e9b23610d55b2c3d9d7b3f6fa9
00-hidan-00 <david.taranov@gmail.com>
2022-07-15 18:33:33 +0200

    :dizzy: Add email-generator

--- email_generator/views.py
+++ email_generator/views.py
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
 from django.http import HttpResponse
 
-from email_generator.utils import generator_email
+from email_generator.services import generator_email
 
 
 def email_generator(request, amount: int = 100):
@@ -10,6 +9,3 @@ def email_generator(request, amount: int = 100):
     new_users = ''.join(formatted_users_list)
     return HttpResponse(f'''<h1> New emails: {len(formatted_users_list)}</h1>
 <ul>{new_users}</ul>''')
-=======
-# Create your views here.
->>>>>>> 26b4e3c (:dizzy: Init email_generator)
