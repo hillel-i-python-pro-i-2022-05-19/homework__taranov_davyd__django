fe8fcc62155245625dde6a25b97f8b872343eaaf
00-hidan-00 <david.taranov@gmail.com>
2022-08-13 19:59:33 +0200

    :toolbox: edit urls.py

--- core/urls.py
+++ core/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('password-generator/', include('password_generator.urls')),
+    path('password-generator/', include('apps.password_generator.urls')),
     path('admin/', admin.site.urls),
 ]
